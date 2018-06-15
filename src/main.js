a8457eb9c5ca17ecd7a412c30fa3e5582cbd1c87
darahat <darahat42@gmail.com>
2018-06-15 10:45:49 +0600

    last commit

--- src/main.js
+++ src/main.js
@@ -5,31 +5,6 @@ import router from './router';
 import store from './store';
 import { CardView } from 'nativescript-cardview';
 import './app.css';
-// import { AD_SIZE, createBanner} from 'nativescript-admob';
- 
-
-// const testing = true;
-// createBanner({
-//   // if this 'view' property is not set, the banner is overlayed on the current top most view
-//   // view: ..,
-//   size: AD_SIZE.SMART_BANNER,
-
-//   androidBannerId: testing
-//       ? "ca-app-pub-5150231094870616~7581615975"  // global test banner id
-//       : "ca-app-pub-5150231094870616/3119912190", // our registered banner id
-//   // Android automatically adds the connected device as test device with testing:true, iOS does not
-//   // iosTestDeviceIds: ["yourTestDeviceUDIDs", "canBeAddedHere"],
-//   margins: {
-//     // if both are set, top wins
-//     // top: 10
- 
-//   },
-//   keywords: ["foo", "bar"]
-// }).then(
-//     () => this.message = "Banner created",
-//     error => this.message = "Error creating banner: " + error
-// )
-
 
 
 require("nativescript-vue").registerElement('CardView', () => CardView);
